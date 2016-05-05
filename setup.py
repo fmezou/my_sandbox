73cc1ae0f443bcea114db397abd386d05b0235cb
Frederic MEZOU <frederic.mezou@free.fr>
2016-05-05 12:46:17 +0200

    dummytask : add a setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='my_sandbox',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='frederic',
+    author_email='',
+    description=''
+)
